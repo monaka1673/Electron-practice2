c44d40ca7b27f6585aebe46e8cbf9c41e1290a56
monaka1673 <ukkari9683@outlook.jp>
2022-08-15 19:06:39 +0900

    change

--- README.md
+++ README.md
@@ -12,5 +12,16 @@
 
 
 
+- DOMContentLoadedイベント
+
+  1.  WebブラウザがグローバルオブジェクトであるWindowオブジェクトを生成
+
+  2. WindowオブジェクトのプロパティとしてDocumentオブジェクトが生成される
+
+  3. DocumentオブジェクトはDOMツリーを読み込む
+
+  4. 解析が終わるとDOMContentLoadedイベントが発火される
+
+
 参考：[「Electron」で簡単デスクトップアプリを制作する勉強会を開催しました！](https://giginc.co.jp/blog/study/1284)
 
--- render.js
+++ render.js
@@ -1,8 +1,3 @@
-// DOMContentLoadedイベント
-// ➀WebブラウザがグローバルオブジェクトであるWindowオブジェクトを生成
-// ➁WindowオブジェクトのプロパティとしてDocumentオブジェクトが生成される
-// ➂DocumentオブジェクトはDOMツリーを読み込む
-// ➃解析が終わるとDOMContentLoadedイベントが発火される
 document.addEventListener('DOMContentLoaded',()=>{
     document.getElementById('comment-form').onsubmit=()=>{
         const commentInput = document.getElementById('comment-input');
